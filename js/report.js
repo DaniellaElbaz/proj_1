d424c3b23d6ae056bb35fcaa7a3c8c2bb2f86aeb
Daniella <daniellaelbaz711@gmail.com>
2024-06-27 01:57:41 +0300

    call the data that selected in the privios page

--- js/report.js
+++ js/report.js
@@ -1,21 +1,21 @@
 let report;
+
 window.onload = () => {
     fetch("../data/Events.json")
     .then(response => response.json())
     .then(data => init_member_details(data));
 }
+
 function init_member_details(data){
-    const name = 'נועה לוינסון';
     let user_photo;
-    let events;
+    const userName = 'נועה לוינסון';
     let user;
     for (const memberKey in data.members) {
         user = data.members[memberKey];
-        if (user.name == name) {
+        if (user.name == userName) {
             user_photo = user.user_photo;
-            events = user.events;
             break;
-            }
+        }
     }
     const userDetails = document.getElementById("UserImage");
     const photo = document.createElement('img');
@@ -25,44 +25,96 @@ function init_member_details(data){
     userDetails.appendChild(photo);
     initReport(user);
 }
+
 function initReport(user) {
     report = document.getElementById("report");
     report.innerHTML = '';
     user.events.forEach(event => {
         const reportItem = document.createElement('div');
         reportItem.classList.add('report-item');
-        const h1 = document.createElement('h1');
-        h1.innerText = `<p style="font-size:12px;">דו"ח אירוע  <span style="font-size:12px;">${event.event_name}</span></p>` ;
-        reportItem.appendChild(h1);
+        const items = inputFromJsonToTextBox(user);
+        if (items) { 
+            reportItem.appendChild(items);
+        }
         const table = document.createElement('table');
         const tr = document.createElement('tr');
         const td = document.createElement('td');
         td.innerHTML = `
-            <p style="font-size:12px;">תאריך: <span style="font-size:12px;">${event.date_and_time}</span></p>
-            <p style="margin-right: 18px;">מקום האירוע: <span style="font-size:12px;">${event.event_place}</span></p>
-            <p style="margin-right: 18px;">סטטוס: <span style="font-size:12px;">${event.event_status}</span></p>`;
-        if (event.event_map) {
-            const td2 = document.createElement('td');
-            const map = document.createElement('img');
-            map.src = event.event_map;
-            map.alt = "Event Map";
-            map.title = "Event_Map";
-            td2.appendChild(map);
-            tr.appendChild(td2);
-        }
+            <p> סוג האירוע</p>
+            <p>שם המשתתף/ת</p>
+            <p>תאריך האירוע</p>`;
         tr.appendChild(td);
+
+        const selectedEvent = showSelectedEvent(user);
+        if (selectedEvent) { 
+            tr.appendChild(selectedEvent);
+        }
         table.appendChild(tr);
         reportItem.appendChild(table);
-        if (event.event_photo) {
-            const eventShow = document.createElement('p');
-            eventShow.innerHTML = ` <p>:צפיה בדוח אירוע / מילוי דוח אירוע</p> `;
-            reportItem.appendChild(eventShow);
-            reportItem.appendChild(initBottomRectangles(event));
-        }
+        
         report.appendChild(reportItem);
     });
 }
-function initBottomRectangles(event) {
+
+function getEventID(){
+    const aKeyValue = window.location.search.substring(1).split('&');
+    const eventId = aKeyValue[0].split("=")[1];
+    return eventId;
+}
+
+function showSelectedEvent(user) {
+    const selectionEventId = getEventID();
+    const name = 'נועה לוינסון';
+    const td = document.createElement('td');
+    let EventType;
+    let EventDate;
+    for (const eventKey in user.event) {
+        let evenDetails = user.event[eventKey];
+        if (evenDetails.id == selectionEventId) {
+            EventType = evenDetails.type_event;
+            const type = document.createElement('input');
+            type.id = "inputType";
+            type.placeholder = EventType;
+            td.appendChild(type);
+            const nameI = document.createElement('input');
+            nameI.id = "userName";
+            nameI.placeholder = name;
+            td.appendChild(nameI);
+            const dateAndTimeParts = evenDetails.date_and_time.trim().split(" שעה ");
+            EventDate = dateAndTimeParts[0];
+            const date = document.createElement('input');
+            date.id = "inputDate";
+            date.placeholder = EventDate;
+            td.appendChild(date);
+            break;
+        }
+    }
+    return td;
+}
+
+function inputFromJsonToTextBox(user){
+    const selectionEventId = getEventID();
+    let reportItem = document.createElement('div');
+    let EventName;
+    let EventPlace;
+    for (const eventKey in user.event) {
+        let evenDetails = user.event[eventKey];
+        if (evenDetails.id == selectionEventId) {
+            EventName = evenDetails.event_name;
+            const h1 = document.createElement('h1');
+            h1.innerText = "דו'ח אירוע " + EventName;
+            reportItem.appendChild(h1);
+            EventPlace = evenDetails.event_place;
+            const h2 = document.createElement('h1');
+            h2.innerText = "מיקום - " + EventPlace;
+            reportItem.appendChild(h2);
+            break;
+        }
+    }
+    return reportItem;
+}
+
+/*function initBottomRectangles(event) {
     const eventLabel = document.createElement('p');
     eventLabel.innerHTML = `:זירת האירוע`;
     const img = document.createElement('img');
@@ -73,5 +125,5 @@ function initBottomRectangles(event) {
     eventContainer.classList.add('event-item-container');
     eventContainer.appendChild(img);
     eventContainer.appendChild(eventLabel);
-    return(eventContainer);
-}
+    return eventContainer;
+}*/
