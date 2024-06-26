909902af51733eb5a4d1c9085b03f87639c7b4f8
Daniella <daniellaelbaz711@gmail.com>
2024-06-26 17:06:27 +0300

    fixe the images position

--- js/event_sort.js
+++ js/event_sort.js
@@ -2,21 +2,23 @@ const sortContainer=document.getElementById('sort-container');
 function fill_sort_container(){
     
     const sortItem = document.createElement('div');
+   
     sortItem.classList.add('sort-item');
-    const box = document.createElement('img');
-    box.src = "../images/checkbox.png";
-    box.alt = "checkbox";
-    box.title = "checkbox";
-    sortItem.appendChild(box);
-    const filter = document.createElement('img');
-    filter.src = "../images/un_filter.png";
-    filter.alt = "filter";
-    filter.title = "filter";
-    sortItem.appendChild(filter);
     const clock = document.createElement('img');
     clock.src = "../images/clock_down.png";
     clock.alt = "clock";
     clock.title = "clock";
     sortItem.appendChild(clock);
+    const filter = document.createElement('img');
+    filter.src = "../images/un_filter.png";
+    filter.alt = "filter";
+    filter.title = "filter";
+    sortItem.appendChild(filter);
+    const box = document.createElement('img');
+    box.src = "../images/checkbox.png";
+    box.alt = "checkbox";
+    box.title = "checkbox";
+    sortItem.appendChild(box);
     sortContainer.appendChild(sortItem);
+    
 }
